7e2ff1f876ef787358739c3dda052053f2710656
Piyushsharma218 <sharmasatyam524@gmail.com>
2026-05-04 02:02:14 +0530

    created the account model and account controller

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const authRouter = require("./routes/auth.routes.js");
+const accountRouter = require("./routes/account.routes.js");
 const cookieParser = require("cookie-parser");
 
 const app = express();
@@ -8,5 +9,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
+app.use("/api/account", accountRouter);
 
 module.exports = app;
---
+++ src/models/account.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const accountSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: [true, "Account must be associated with user"],
+      index: true,
+    },
+    status: {
+      type: String,
+      enum: {
+        values: ["ACTIVE", "FROZEN", "CLOSED"],
+        message: "status can be either ACTIVE, FROZEN or CLOSED",
+      },
+      default: "ACTIVE",
+    },
+    currency: {
+      type: String,
+      required: [true, "currency is required for creating an account"],
+      default: "INR",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+accountSchema.index({ user: 1, status: 1 });
+
+const accountModel = mongoose.model("account", accountSchema);
+
+module.exports = accountModel;
